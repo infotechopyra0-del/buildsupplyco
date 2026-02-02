d3f3f64df51abb22abe97c072ba34f345d00ae35
ashmittsingh <ashmitsingh1310@gmail.com>
2026-02-02 22:45:44 +0530

    Add New Features

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -31,7 +31,7 @@ export default function FAQsPage() {
           </p>
         </motion.div>
       </section>
-
+      
       {/* FAQs Section */}
       <section className="w-full py-16 lg:py-24">
         <div className="max-w-4xl mx-auto px-8 lg:px-16">
